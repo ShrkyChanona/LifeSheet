f0deb2a8ebe44d1e766b24374780627ee2b1670e
Carlos Eduardo Chanona Aquino <62265673+ShrkyChanona@users.noreply.github.com>
2023-01-19 13:25:00 -0600

    add header

---
+++ src/components/atoms/Name.jsx
@@ -0,0 +1,14 @@
+import '../../assets/styles/Name.css'
+
+function Name() {
+    return ( 
+        <div id='name-title'>
+            <h1>
+            CARLOS EDUARDO <br />
+            CHANONA AQUINO
+        </h1>
+        </div>
+     );
+}
+
+export default Name;
---
+++ src/pages/LifeSheet.jsx
@@ -0,0 +1,15 @@
+import ProfilePic from "../components/molecules/ProfilePic";
+import Name from "../components/atoms/Name";
+
+function LifeSheet() {
+    return ( 
+        <>
+            <header>
+                <ProfilePic/>
+                <Name/>
+            </header>
+        </>
+     );
+}
+
+export default LifeSheet;
